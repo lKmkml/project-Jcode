e4cff658cdbda715010f3ca0e607d9de699c3ebd
Pipatpong Thadapitakporn <51953443+lKmkml@users.noreply.github.com>
2023-01-18 08:07:53 +0700

    savemodels

--- app/models.py
+++ app/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    def __str__(self):
+        return self.name
+
+class CategorySub(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    def __str__(self):
+        return self.name
+
+
+
